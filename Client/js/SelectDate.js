e4b9f2e2e02bca4c7cd8c62fb60d8dc5a7c3119d
niices <876394575@qq.com>
2014-08-14 16:43:32 +0800

    client

---
+++ Client/js/SelectDate.js
@@ -0,0 +1,45 @@
+﻿function BindDateTime(Yearid,Monthid,Dayid)
+{
+    var minYear = new Date().getFullYear() ; //最小年
+    var maxYear = new Date().getFullYear() ; //最大年
+    for(minYear;minYear<=maxYear;minYear++)
+    {
+        $("#"+Yearid).addOption(minYear,minYear);
+    }
+    for(var m=1;m<=12;m++)
+    {
+        $("#"+Monthid).addOption(m,m);
+    }
+    $("#"+Yearid).bind("change",function(){
+        GetDays(Yearid,Dayid,$("#"+Monthid).val());
+    });
+    $("#"+Monthid).bind("change",function(){
+        GetDays(Yearid,Dayid,this.value);
+    });
+    GetDays(Yearid,Dayid,1);  //默认值
+}
+
+function GetDays(Yearid,Dayid,Month)
+{
+    var obj = $("#"+Dayid);
+    obj.clearAll();
+    var MonthDay = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
+    for(var d=1;d<=MonthDay[Month-1];d++)
+    {
+        obj.addOption(d,d);
+    }
+    if(!IsPinYear($("#"+Yearid).val()) && Month == 2) obj.addOption(29,29);
+}
+
+function IsPinYear(year)
+{
+    return (0 == year%4 && (year%100 !=0 || year%400 == 0))
+}
+
+function BindDateValue(Ids,Values)
+{
+    for(var i=0;i<Ids.length;i++)
+    {
+        $("#"+Ids[i]).val(Values[i]);
+    }
+}
---
+++ Client/js/style.js
@@ -0,0 +1,7 @@
+/*
+* @Author: 蒋恩涛
+* @Date:   2014-08-12 16:40:03
+* @Last Modified by:   蒋恩涛
+* @Last Modified time: 2014-08-13 13:00:43
+*/
+
